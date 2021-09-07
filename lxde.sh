5543544c9326e4d7c16648d8f5907dad9c7ebb1a
rhexa <akun.kripitit@gmail.com>
2021-09-07 21:07:23 +0300

    modified lxde script

--- lxde.sh
+++ lxde.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
 # echo 'DPkg::options { "--force-confdef"; };' | sudo tee /etc/apt/apt.conf
-sudo apt install -y lxde-core
-sudo apt remove -y gdm3
-sudo apt install -y lightdm
+sudo apt install -y lxde-core lightdm
