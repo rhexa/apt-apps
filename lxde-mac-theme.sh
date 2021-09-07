92c149837e03c894ad1db687d81d4d0db217871f
rhexa <akun.kripitit@gmail.com>
2021-09-07 17:23:57 +0300

    modified file

--- lxde-mac-theme.sh
+++ lxde-mac-theme.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-wget --content-disposition https://raw.githubusercontent.com/rhexa/apt-lxde-mac-theme/main/lxde-mac-theme.sh
-chmod u+x lxde-mac-theme.sh
-./lxde-mac-theme.sh
+wget --content-disposition https://raw.githubusercontent.com/rhexa/apt-lxde-mac-theme/main/install.sh
+chmod u+x install.sh
+./install.sh
